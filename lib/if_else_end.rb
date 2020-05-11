1fc389def73c6556ae99a71d54b4bcbe5c4f0d39
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-11 20:49:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,8 @@
 # Write your solution here
-current_time = Time.now
-puts current_time.to_i.class
+current_time = Time.now.to_i
+is_even = current_time % 2 == 0 ? true : false
+if(is_even)
+  puts "Even!"
+else
+  puts "Odd!"
+
